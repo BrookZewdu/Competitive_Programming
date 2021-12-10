a50b13bbbf0d0774dcaaf572b89a286e2c7177ec
Biruk Zewdu <88080440+BrookZewdu@users.noreply.github.com>
2021-12-10 15:01:46 +0300

    Create Domino_piling.java

---
+++ week1/Domino_piling/Domino_piling.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Domino_pile{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int M, N;
+		M = sc.nextInt();
+		N = sc.nextInt();
+
+		System.out.println(M*N/2);
+
+	}
+}
