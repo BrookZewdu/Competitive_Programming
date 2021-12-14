c4a371e041385cc25705d2935275f156dc109436
Biruk Zewdu <88080440+BrookZewdu@users.noreply.github.com>
2021-12-14 19:10:28 +0300

    Update Kth_largest_num.java

--- week2/Kth_largest_num/Kth_largest_num.java
+++ week2/Kth_largest_num/Kth_largest_num.java
@@ -1,1 +1,14 @@
 
+class Solution {
+    public String kthLargestNumber(String[] nums, int k) {
+      //comparator
+        Arrays.sort(nums, (a,b) -> {
+            if (a.length() == b.length()) {
+                return b.compareTo(a);
+            }
+            return b.length() - a.length();
+        });
+        return nums[k - 1];
+    }
+    
+}
