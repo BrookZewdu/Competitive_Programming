4641de1e6e1095954d5d59c69d40ac9b0f63366b
Biruk Zewdu <88080440+BrookZewdu@users.noreply.github.com>
2022-03-04 17:21:26 +0300

    Update RottingOranges.java

--- A2SV GROUP 3 - CAMP PROBLEMS/994. Rotting Oranges - LeetCode Problem/RottingOranges.java
+++ A2SV GROUP 3 - CAMP PROBLEMS/994. Rotting Oranges - LeetCode Problem/RottingOranges.java
@@ -1,57 +1,57 @@
 class Solution {
-    private int count = 0;
-    int temp = 0;
-    int max = 0;
-    Queue<Integer> q = new LinkedList<>();
-    int grid[][];
-    int width;
     public int orangesRotting(int[][] grid) {
-        this.width = grid[0].length;
-        this.grid = grid;
+        // time complexity -> O(row * col)
+        // space complexity -> O(row * col); 
+        class tupple <tupple>{
+            int row, col, rotten_time;
+            public tupple(int row, int col, int rotten_time){
+                this.row = row;
+                this.col = col;
+                this.rotten_time = rotten_time;
+            }
+        }
+        int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        Queue<tupple> q = new LinkedList<>();
+        int minutes = 0;
+        int ones = 0;
+        
         for(int i = 0; i < grid.length; i++){
             for(int j = 0; j < grid[0].length; j++){
                 if(grid[i][j] == 2){
-                   q.add(i * width + j); 
+                    q.add(new tupple(i,j,0));
+                }else if(grid[i][j] == 1){
+                    ones++;
                 }
+                
             }
         }
-        bfs();
-        for(int i = 0; i < grid.length; i++){
-            for(int j = 0; j < grid[0].length; j++){
-                if(grid[i][j] == 1){
-                    return -1;
-                }
-            }
-        }
-        return count == 0 ? 0 : count - 1;
-    }
-    private void bfs(){
+        
         while(!q.isEmpty()){
             int n = q.size();
             for(int i = 0; i < n; i++){
-                int r = q.peek() / width;
-                int c = q.poll () % width;
-                
-                if(r > 0 && grid[r - 1][c] == 1) {
-                    q.add((r - 1) * width + c);
-                    grid[r - 1][c] = 2;
-                }
-                if(c > 0 && grid[r][c - 1] == 1) {
-                    q.add(r * width + (c - 1));
-                    grid[r][c - 1] = 2;
-                }
-                if(r < grid.length - 1 && grid[r + 1][c] == 1) {
-                    q.add((r + 1) * width + c);
-                    grid[r + 1][c] = 2;
-                }
-                if(c < grid[0].length - 1 && grid[r][c + 1] == 1) {
-                    q.add(r * width + (c + 1)); 
-                    grid[r][c + 1] = 2;
-                }
+                int row = q.peek().row;
+                int col = q.peek().col;
+                int rotten_time = q.poll().rotten_time;
+                minutes = rotten_time;
+                for(int[] direction : dirs){
+                    int new_row = row + direction[0];
+                    int new_col = col + direction[1];
                     
+                    if(isValid(grid, new_row, new_col) && grid[new_row][new_col] == 1){
+                        q.add(new tupple(new_row, new_col, rotten_time + 1));
+                        grid[new_row][new_col] = 2;
+                        ones--;
+                    }
+                }
+                
             }
-            count++;
+            
         }
+        return (ones == 0) ? minutes : -1;
         
+    }
+    private boolean isValid(int[][] grid, int row, int col){ 
+        if(row >= 0 && col >= 0 && row < grid.length && col < grid[0].length) return true;
+        return false;
     }
 }
