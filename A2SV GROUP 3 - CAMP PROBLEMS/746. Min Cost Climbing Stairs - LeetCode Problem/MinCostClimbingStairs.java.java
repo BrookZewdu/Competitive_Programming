510a14e5a59a07fc313b37d6def8a8986888d054
BrookZewdu <brookzewdu09@gmail.com>
2022-03-09 16:33:54 +0300

    created MinCostClimbingStairs.java

---
+++ A2SV GROUP 3 - CAMP PROBLEMS/746. Min Cost Climbing Stairs - LeetCode Problem/MinCostClimbingStairs.java.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        for (int i = cost.length - 3; i >= 0; i--) {
+            cost[i] = cost[i] + Math.min(cost[i + 1], cost[i + 2]);
+        }
+        return Math.min(cost[0], cost[1]);
+    }
+}
